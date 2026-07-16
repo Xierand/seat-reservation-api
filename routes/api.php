db25aae6c5c75b19235d3bfb69b623e7f30bbf6f
Xierand <xierand7@gmail.com>
2026-07-16 14:16:23 +0200

    refactor: replace resource route with apiResource for events endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\SectorController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::resource('events', EventController::class);
+    Route::apiResource('events', EventController::class);
     Route::apiResource('events.sectors', SectorController::class)->scoped();
 });
