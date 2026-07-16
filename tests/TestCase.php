c3d9de2aa6386587c04277b37f3d7870c07c4487
Xierand <xierand7@gmail.com>
2026-07-16 15:42:08 +0200

    test: set up internal API key header for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withHeader(
+            'X-Internal-Api-Key',
+            (string) config('services.internal.api_key')
+        );
+    }
 }
