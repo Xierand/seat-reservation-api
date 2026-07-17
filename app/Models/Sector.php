1503010c6803228586d370ccd33011d390aaf149
Xierand <xierand7@gmail.com>
2026-07-17 14:50:19 +0200

    minor: add seats relationship to Sector model

--- app/Models/Sector.php
+++ app/Models/Sector.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable([
     'event_id',
@@ -28,5 +29,11 @@ class Sector extends Model
     public function event(): BelongsTo
     {
         return $this->belongsTo(Event::class);
+    }
+
+    public function seats(): HasMany
+    {
+        return $this->hasMany(Seat::class);
+
     }
 }
